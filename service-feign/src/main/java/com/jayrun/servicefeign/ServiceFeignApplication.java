7b86ab6b018da217c1ccb6163e46d027f5db28e9
Jayrun <jayrun_z@163.com>
2018-10-09 18:44:00 +0800

    短路监控

--- service-feign/src/main/java/com/jayrun/servicefeign/ServiceFeignApplication.java
+++ service-feign/src/main/java/com/jayrun/servicefeign/ServiceFeignApplication.java
@@ -2,14 +2,22 @@ package com.jayrun.servicefeign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableEurekaClient
 @EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+@EnableTurbine
 public class ServiceFeignApplication {
 
 	public static void main(String[] args) {
