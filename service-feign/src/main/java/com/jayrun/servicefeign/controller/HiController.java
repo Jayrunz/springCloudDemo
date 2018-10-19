4cb42cd8fcd90c94cae4e23c1e5272ac3819230a
Jayrun <jayrun_z@163.com>
2018-10-19 10:55:08 +0800

    调server测试

--- service-feign/src/main/java/com/jayrun/servicefeign/controller/HiController.java
+++ service-feign/src/main/java/com/jayrun/servicefeign/controller/HiController.java
@@ -1,8 +1,10 @@
 package com.jayrun.servicefeign.controller;
 
 import com.jayrun.servicefeign.service.FeignHelloService;
+import com.jayrun.servicefeign.service.ServerHelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +14,16 @@ public class HiController {
     @Autowired
     private FeignHelloService feignHelloService;
 
+    @Autowired
+    private ServerHelloService serverHelloService;
+
     @GetMapping("/hi")
     public String sayHiWithFeign(@RequestParam String name) {
         return feignHelloService.sayHiWithFeign(name) + " from feign";
+    }
+
+    @GetMapping("/server/{name}")
+    public String helloServer(@PathVariable("name") String name) {
+        return serverHelloService.helloServer(name);
     }
 }
